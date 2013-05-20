1f7b2e8e9bbe21b271dadfb0e359fd7c8a3e1288
Charlotte Cameron <charlottemidgley@gmail.com>
2013-05-20 16:12:11 +0100

    Completed ruby exercise 7.

---
+++ ex7.rb
@@ -0,0 +1,27 @@
+# The following 3 lines print their strings on new lines, the second line has another string inside
+puts "Mary had a little lamb."
+puts "Its fleece was white as %s." % 'snow'
+puts "And everywhere that Mary went."
+# Puts 10 full stops after on another on a line
+puts "." * 10 # what'd that do?
+
+# Declare the variables end1 to end12 as individual characters
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+#Printing variables end1 to end6 as one string on one line, and then variables end7 to end8. All on one line.
+print end1 + end2 + end3 + end4 + end5 + end6
+print end7 + end8 + end9 + end10 + end11 + end12
+
+# This puts the terminal command promt on a new line
+puts
