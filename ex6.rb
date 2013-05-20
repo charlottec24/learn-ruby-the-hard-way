099432f8fdce9aaad25934b4921289bad533e9cf
Charlotte Cameron <charlottemidgley@gmail.com>
2013-05-20 15:46:45 +0100

    Completed ruby exercise 6

---
+++ ex6.rb
@@ -0,0 +1,33 @@
+# Declaring variable x as the below string with another string inside it #{10}
+x = "There are #{10} types of people."
+# Decalring the variable binary as the below string
+binary = "binary"
+# Declaring varible do_not as the below string
+do_not = "don't"
+# Declaring variable y as the below string with variables binary and do_not also included.
+y = "Those who know #{binary} and those who #{do_not}."
+
+# Print variable x on a new line
+puts x
+# Print variable y on a new line
+puts y
+
+# Print the below string on a new line with variable x also included
+puts "I said: #{x}."
+# Print the below string on a new line with variable y also included
+puts "I also said: '#{y}'."
+
+# Delaring the variable hilarious as false
+hilarious = false
+# Declaring the variable joke_evaluation as the following string including variable hilarious
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+# Print variable joke_evaluation on a new line
+puts joke_evaluation
+
+# Declare variables w and e as the two strings below
+w = "This is the left side of..."
+e = "a string with a right side."
+
+# Print variables w and e after one another on a new line
+puts w + e
