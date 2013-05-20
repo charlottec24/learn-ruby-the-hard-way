c990cb1f8b006f49864d7c15225c7650e0aa7d16
Charlotte Cameron <charlottemidgley@gmail.com>
2013-05-20 18:28:26 +0100

    Completed ruby exercise 11.

---
+++ ex11.rb
@@ -0,0 +1,15 @@
+print "How old are you? "
+age = gets.chomp()
+print "How tall are you? "
+height = gets.chomp()
+print "How much do you weigh? "
+weight = gets.chomp()
+
+puts "So you're #{age} years old, #{height} tall and #{weight} heavy."
+
+print "Where are you from? "
+origin = gets.chomp()
+print "Who do you know? "
+friend = gets.chomp()
+
+puts "Ahh, so you are from #{origin} and know #{friend}."
