b105cabab564350174032c6a2d575294671c327d
Charlotte Cameron <charlottemidgley@gmail.com>
2013-05-20 13:23:17 +0100

    Completed extra credit for ruby exercise 3

--- ex3.rb
+++ ex3.rb
@@ -8,26 +8,36 @@
 # <= less-than-equal
 # >= greater-than-equal
 
+# Prints the below line on a new line
 puts "I will now count my chickens:"
 
-puts "Hens", 25 + 30 / 6
-puts "Roosters", 100 - 25 * 3 % 4
+# Prints Hens on a new line and then run the following calulation
+puts "Hens", 25.0 + 30.0 / 6.0
+puts "Roosters", 100.0 - 25.0 * 3.0 % 4.0
 
+# Print the string on a new line
 puts "Now I will count the eggs:"
 
-puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# Works out the following calculation
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
+# Prints the following as a string
 puts "Is it true that 3 + 2 < 5 - 7?"
 
-puts 3 + 2 < 5 - 7
+# Works out calculation
+puts 3.0 + 2.0 < 5.0 - 7.0
 
-puts "What is 3 + 2?", 3 + 2
-puts "What is 5 - 7?", 5 - 7
+# Prints string, then works out calculation
+puts "What is 3 + 2?", 3.0 + 2.0
+puts "What is 5 - 7?", 5.0 - 7.0
 
+# Prints the following as a string
 puts "Oh, that's why it's false."
 
+# Prints the following as a string
 puts "How about some more."
 
-puts "Is it greater?", 5 > -2
-puts "Is it greater or equal?", 5 >= -2
-puts "Is it less or equal?", 5 <= -2
+# Prints string, then works out calculation
+puts "Is it greater?", 5.0 > -2.0
+puts "Is it greater or equal?", 5.0 >= -2.0
+puts "Is it less or equal?", 5.0 <= -2.0
