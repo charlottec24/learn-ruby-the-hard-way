1fc7692a316adbaf4255c617d513725206e989dd
Charlotte Cameron <charlottemidgley@gmail.com>
2013-05-22 17:03:44 +0100

    Completed ruby exercise 33.

---
+++ ex33.rb
@@ -0,0 +1,44 @@
+i = 0
+numbers = []
+
+while i < 6
+	puts "At the top i is #{i}"
+	numbers.push(i)
+
+	i = i + 1
+	puts "Numbers now: #{numbers}"
+	puts "At the bottom i is #{i}"
+end
+
+puts "The numbers: "
+
+for num in numbers
+	puts num
+end
+
+# Lean-Mean-Fruit-Machine-II:learn-ruby-the-hard-way Charlotte$ ruby ex33.rb
+# At the top i is 0
+# Numbers now: [0]
+# At the bottom i is 1
+# At the top i is 1
+# Numbers now: [0, 1]
+# At the bottom i is 2
+# At the top i is 2
+# Numbers now: [0, 1, 2]
+# At the bottom i is 3
+# At the top i is 3
+# Numbers now: [0, 1, 2, 3]
+# At the bottom i is 4
+# At the top i is 4
+# Numbers now: [0, 1, 2, 3, 4]
+# At the bottom i is 5
+# At the top i is 5
+# Numbers now: [0, 1, 2, 3, 4, 5]
+# At the bottom i is 6
+# The numbers: 
+# 0
+# 1
+# 2
+# 3
+# 4
+# 5
